33d1ed736b253a55f6904de7695c3576c8a5ff53
Iskender Degenbaev <113851731+IskeenX@users.noreply.github.com>
2025-03-15 01:20:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function loadSection(section) {
-    fetch(`/Pages/${section}.html`)
+    fetch(`Pages/${section}.html`)
         .then(response => response.text())
         .then(data => {
             document.getElementById('content').innerHTML = data;
+            document.querySelectorAll("nav a").forEach(a => a.classList.remove("active"));
+            document.querySelector(`nav a[onclick="loadSection('${section}')"]`).classList.add("active");
         });
 }
